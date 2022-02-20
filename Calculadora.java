e7d141043dfe4c3699a9b1ed2f7ba977f5041026
erickguerra22 <erickguerra2201@gmail.com>
2022-02-19 23:33:54 -0600

    Versión 2

--- Calculadora.java
+++ Calculadora.java
@@ -1,17 +1,17 @@
 public class Calculadora implements IPosfixCalc{
 	private char[] operators = new char[]{'-','+','*','/'};
-	StackArrayList<Integer> lista;
+	StackArrayList<Double> lista;
 
 	@Override
 	public int Evaluate(String expresion) {
-		lista = new StackArrayList<Integer>(); //Se cre una nueva pila de tipo Double
+		lista = new StackArrayList<Double>(); //Se cre una nueva pila de tipo Double
 		expresion = expresion.replace(" ", ""); //Remover espacios en blanco del texto
 		for(int i = 0; i<expresion.length();i++) {
 			if(!isANumber(expresion.charAt(i)) && !(new String(operators).contains(Character.toString(expresion.charAt(i))))) // Se comprueba que el caracter correspondiente es un número o un operador
 				return -1;
 			else if(isANumber(expresion.charAt(i))) {
 				String numberText = Character.toString(expresion.charAt(i));
-				int number = Integer.parseInt(numberText);
+				double number = Integer.parseInt(numberText);
 				lista.push(number); // En caso de ser un número, se guarda en la pila.
 			}else {
 				if(lista.count()<2)
@@ -24,7 +24,7 @@ public class Calculadora implements IPosfixCalc{
 					}
 			}
 		}
-		return lista.peek();
+		return (int)Math.round(lista.peek()); //Se envía el entero aproximado del resultado.
 	}
 	
 	//Evalúa si un caracter indicado es un número entero
@@ -40,8 +40,8 @@ public class Calculadora implements IPosfixCalc{
 	
 	//Ejecuta la operación indicada, según el operador.
 	private void operate(char operator) throws ArithmeticException{
-		int num1 = 0;
-		int num2 = 0;
+		double num1 = 0;
+		double num2 = 0;
 		switch(operator) {
 		case '+':
 			num1 = lista.pull();
@@ -54,10 +54,12 @@ public class Calculadora implements IPosfixCalc{
 			lista.push(num2-num1);
 			break;
 		case '/':
-			int resultado = 0;
+			double resultado = 0;
 			num1 = lista.pull();
 			num2 = lista.pull();
 			resultado = num2/num1;
+			if(Double.isInfinite(resultado))
+				throw new ArithmeticException("/ by 0"); //Si intenta dividir entre 0,se genera una excepción aritmética.
 			lista.push(resultado);
 			break;
 		case '*':
